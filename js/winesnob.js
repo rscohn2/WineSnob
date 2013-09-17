53469866a656324d7196851ee515d57406362dc0
Robert Cohn <robert.s.cohn@intel.com>
2013-09-17 13:59:50 -0400

    add data for wines

---
+++ js/winesnob.js
@@ -0,0 +1,36 @@
+var wineData = [
+    {
+        name: 'winea',
+        varietal: 'varietya',
+        appelation: 'appelationa',
+        glassPrice: 1,
+        storeBottlePrice: 10,
+        restaurantBottlePrice: 20,
+        rating: 4,
+        notes: 'Some notes about winea',
+        photo: null
+    },
+    {
+        name: 'wineb',
+        varietal: 'varietyb',
+        appelation: 'appelationb',
+        glassPrice: 2,
+        storeBottlePrice: 11,
+        restaurantBottlePrice: 21,
+        rating: 3,
+        notes: 'Some notes about wineb',
+        photo: null
+    },
+    {
+        name: 'winec',
+        varietal: 'varietyc',
+        appelation: 'appelationc',
+        glassPrice: 3,
+        storeBottlePrice: 11,
+        restaurantBottlePrice: 21,
+        rating: 2,
+        notes: 'Some notes about winec',
+        photo: null
+    }
+];
+
